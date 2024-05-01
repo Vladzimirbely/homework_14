6594fa2c9ab53b4473a475724cc5838fd65eeb58
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-05-01 15:46:28 +0300

    write tests with params

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -1,17 +1,29 @@
-"""
-Переопределите параметр с помощью indirect параметризации на уровне теста
-"""
 import pytest
+from pages.github import GithubPage
+from selene import browser
 
+@pytest.fixture(params=['desktop', 'mobile'])
+def driver(request):
+    if request.param == 'desktop':
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+    else:
+        browser.config.window_width = 400
+        browser.config.window_height = 700
 
-@pytest.fixture()
-def browser():
-    pass
+    yield
+    browser.quit()
 
+@pytest.mark.parametrize('driver', ['desktop'], indirect=True)
+def test_github_desktop(driver):
+    github_page = GithubPage()
+    github_page.open()
+    github_page.click_on_sign_in_desktop()
+    github_page.should_have_text_sign_in()
 
-def test_github_desktop(browser):
-    pass
-
-
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize('driver', ['mobile'], indirect=True)
+def test_github_mobile(driver):
+    github_page = GithubPage()
+    github_page.open()
+    github_page.click_on_sign_in_mobile()
+    github_page.should_have_text_sign_in()
